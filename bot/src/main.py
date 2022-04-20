396631b8364627c8db0415f7de76b24264682015
christos michail <christos.m.michael@gmail.com>
2022-04-20 20:21:48 +0300

    compose fix server

--- bot/src/main.py
+++ bot/src/main.py
@@ -7,7 +7,7 @@ from telegram import BotCommand, Update, Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 APP_ID = os.environ['APP_ID']
-SERVER = f"{os.environ['SERVER']}/wenet"
+SERVER = os.environ['SERVER']
 WENET_WEBSITE = os.environ['WENET_WEBSITE']
 WENET_AUTHENTICATION = os.environ['WENET_AUTHENTICATION']
 
