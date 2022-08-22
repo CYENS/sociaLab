b7c6ee1ac22bb8d83036547b93122e7ce6a49a00
christos michail <christos.m.michael@gmail.com>
2022-08-22 15:28:42 +0300

    Update settings.py

--- socialbot/socialbot/settings.py
+++ socialbot/socialbot/settings.py
@@ -52,6 +52,27 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'propagate': False,
+        },
+    },
+}
+
 ROOT_URLCONF = 'socialbot.urls'
 
 TEMPLATES = [
