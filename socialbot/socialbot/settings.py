a37436c1930a04d1da3701ca1f829780ebe78205
christos michail <christos.m.michael@gmail.com>
2022-09-07 12:52:29 +0300

    Update settings.py

--- socialbot/socialbot/settings.py
+++ socialbot/socialbot/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+filepath = Path(__file__).parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'socialbot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
