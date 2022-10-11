5fc91d5fd26faa32cde9f7a128cbc91f8e77f133
christos michail <christos.m.michael@gmail.com>
2022-10-11 13:59:06 +0300

    Update views.py

--- socialbot/wenet_webhooks/views.py
+++ socialbot/wenet_webhooks/views.py
@@ -324,8 +324,9 @@ def _send_answer_to_user(answer: Answer):
             KeyboardButton("Yes"),
             KeyboardButton("Noo")
         ]
+        reply=0
         bot.send_message(1595070759,"hey",parse_mode=ParseMode.MARKDOWN_V2)
-        reply=bot.send_message(1595070759, reply_markup=ReplyKeyboardMarkup.from_column(markup_list, one_time_keyboard=True))
+        bot.send_message(1595070759, reply_markup=ReplyKeyboardMarkup.from_column(markup_list, one_time_keyboard=True))
         logger.info(reply)
         if "Yes" in reply:
             bot.send_message(1595070759, "ok marked as solved!",
