dbc6231aa676c1ad47f54911e070e15050ecdfa2
christos michail <christos.m.michael@gmail.com>
2022-10-17 19:36:08 +0300

    Update admin.py

--- socialbot/wenet_webhooks/admin.py
+++ socialbot/wenet_webhooks/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Question, Answer
+from .models import User, Question, Answer,Best_Answer
 
 admin.site.register(User)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Best_Answer)
