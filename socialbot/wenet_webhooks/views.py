73b4d76daaf93331cba3b4e6cb686fbdb97f4526
christos michail <christos.m.michael@gmail.com>
2022-10-21 19:37:01 +0300

    Update views.py

--- socialbot/wenet_webhooks/views.py
+++ socialbot/wenet_webhooks/views.py
@@ -339,12 +339,12 @@ def _send_answer_to_user(answer: Answer):
 @csrf_exempt
 def set_best_answer(request: HttpRequest):
     try:
-        question_id = request.POST['question_id']
-        answer_id = request.POST['answer_id']
-        logger.info("*****"+str(question_id))
-        logger.info(answer_id)
-        best_answer = Best_Answer(question=question_id, answer=answer_id)
-        best_answer.save
+        # question_id = request.POST['question_id']
+        # answer_id = request.POST['answer_id']
+        # logger.info("*****"+str(question_id))
+        # logger.info(answer_id)
+        # best_answer = Best_Answer(question=question_id, answer=answer_id)
+        # best_answer.save
         return HttpResponse()
     except Exception as e:
         logger.info('_send_answer failed')
